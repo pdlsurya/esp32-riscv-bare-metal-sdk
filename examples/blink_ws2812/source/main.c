f3be619665042cd8af4679c6c00ba7311381d7c8
Surya Poudel <poudel.surya2011@gmail.com>
2026-01-23 23:29:12 +0545

    Add esp_hosted and blink_ws2812 examples

---
+++ examples/blink_ws2812/source/main.c
@@ -0,0 +1,55 @@
+#include <stdbool.h>
+#include <stdint.h>
+
+#include "delay.h"
+#include "gpio_drv.h"
+#include "usb_serial.h"
+#include "ws2812.h"
+
+#define LED_GPIO_PIN 2U
+
+int main(void)
+{
+    uint32_t step = 0U;
+
+    delay_ms(200U);
+    serial_printf("ESP32-P4 blink + WS2812 example\n");
+
+    gpio_set_direction(LED_GPIO_PIN, GPIO_OUTPUT);
+    gpio_write(LED_GPIO_PIN, 0U);
+
+    ws2812_init();
+
+    while (1)
+    {
+        gpio_toggle(LED_GPIO_PIN);
+
+        switch (step & 0x3U)
+        {
+            case 0U:
+                ws2812_write((ws2812_color_t)WS2812_COLOR_RED);
+                serial_printf("[blink_ws2812] led=RED\n");
+                break;
+
+            case 1U:
+                ws2812_write((ws2812_color_t)WS2812_COLOR_GREEN);
+                serial_printf("[blink_ws2812] led=GREEN\n");
+                break;
+
+            case 2U:
+                ws2812_write((ws2812_color_t)WS2812_COLOR_BLUE);
+                serial_printf("[blink_ws2812] led=BLUE\n");
+                break;
+
+            default:
+                ws2812_write((ws2812_color_t)WS2812_COLOR_BLACK);
+                serial_printf("[blink_ws2812] led=OFF\n");
+                break;
+        }
+
+        step++;
+        delay_ms(500U);
+    }
+
+    return 0;
+}
---
+++ examples/esp_hosted/build.sh
@@ -0,0 +1,3 @@
+#rm -rf build
+cmake  -Bbuild -GNinja -DTARGET_SOC=esp32p4
+cmake --build build && esptool.py  write_flash 0x0 build/app.bin
